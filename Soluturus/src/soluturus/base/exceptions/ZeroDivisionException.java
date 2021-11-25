de38068cb74dc9e9b4c412402fb5d062dca960a9
mkbertrand <94151976+mkbertrand@users.noreply.github.com>
2021-11-24 19:41:02 -0500

    Moved exceptions to separate package

---
+++ Soluturus/src/soluturus/base/exceptions/ZeroDivisionException.java
@@ -0,0 +1,20 @@
+package soluturus.base.exceptions;
+
+/**
+ * Thrown when a method attempts to divide by zero.
+ * 
+ * @author Miles K. Bertrand
+ * 
+ */
+public class ZeroDivisionException extends ArithmeticException {
+
+	private static final long serialVersionUID = -6213316378633013147L;
+
+	public ZeroDivisionException() {
+		super();
+	}
+
+	public ZeroDivisionException(String cause) {
+		super(cause);
+	}
+}
