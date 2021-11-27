c3d462fc8f46d0651b19c40281e1f9b436fa5d24
mkbertrand <94151976+mkbertrand@users.noreply.github.com>
2021-11-26 19:06:18 -0500

    Complete simplifier revamp

--- Soluturus/src/soluturus/utils/internal/Simplifier.java
+++ Soluturus/src/soluturus/utils/internal/Simplifier.java
@@ -1,18 +1,16 @@
 package soluturus.utils.internal;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.TreeMap;
-import java.util.function.Predicate;
+import java.util.HashMap;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
+import soluturus.base.Trigonometric;
 import soluturus.base.expressions.Expression;
 import soluturus.base.expressions.Integer;
 import soluturus.base.expressions.Variable;
-import soluturus.calculations.Fraction;
 
 /**
  * The Simplifier converts strings and other formats into operable expressions.
@@ -24,22 +22,63 @@ import soluturus.calculations.Fraction;
  */
 public final class Simplifier {
 
-	static final TreeMap<Character, Expression> COMMON_VALUES = new TreeMap<>();
-	static final TreeMap<String, Shortcut> SHORTCUTS = new TreeMap<>();
-	static final Predicate<Character> NUMBER_CHARACTER = c -> Character.getType(c) == Character.DECIMAL_DIGIT_NUMBER
-			|| c == '-' || c == '.';
-
-	private static final Object lock = new Object();
+	private static final HashMap<String, Character> greekletters = new HashMap<String, Character>();
+	private static final HashMap<String, Function<Expression[], Expression>> functions = new HashMap<String, Function<Expression[], Expression>>();
 
 	static {
+		greekletters.put("Alpha", '\u0391');
+		greekletters.put("alpha", '\u03B1');
+		greekletters.put("Beta", '\u0392');
+		greekletters.put("beta", '\u03B2');
+		greekletters.put("Gamma", '\u0393');
+		greekletters.put("gamma", '\u03B3');
+		greekletters.put("Delta", '\u0394');
+		greekletters.put("delta", '\u03B4');
+		greekletters.put("Epsilon", '\u0395');
+		greekletters.put("epsilon", '\u03B5');
+		greekletters.put("Zeta", '\u0396');
+		greekletters.put("zeta", '\u03B6');
+		greekletters.put("Eta", '\u0397');
+		greekletters.put("eta", '\u03B7');
+		greekletters.put("Theta", '\u0398');
+		greekletters.put("theta", '\u03B8');
+		greekletters.put("Iota", '\u0399');
+		greekletters.put("iota", '\u03B9');
+		greekletters.put("Kappa", '\u039A');
+		greekletters.put("kappa", '\u03BA');
+		greekletters.put("Lambda", '\u039B');
+		greekletters.put("lambda", '\u03BB');
+		greekletters.put("Mu", '\u039C');
+		greekletters.put("mu", '\u03BC');
+		greekletters.put("Nu", '\u039D');
+		greekletters.put("nu", '\u03BD');
+		greekletters.put("Xi", '\u039E');
+		greekletters.put("xi", '\u03BE');
+		greekletters.put("Omicron", '\u039F');
+		greekletters.put("omicron", '\u03BF');
+		greekletters.put("Pi", '\u03A0');
+		greekletters.put("pi", '\u03C0');
+		greekletters.put("Rho", '\u03A1');
+		greekletters.put("rho", '\u03C1');
+		greekletters.put("Sigma", '\u03A3');
+		greekletters.put("sigma", '\u03C3');
+		greekletters.put("Tau", '\u03A4');
+		greekletters.put("tau", '\u03C4');
+		greekletters.put("Upsilon", '\u03A5');
+		greekletters.put("upsilon", '\u03C5');
+		greekletters.put("Phi", '\u03A6');
+		greekletters.put("phi", '\u03C6');
+		greekletters.put("Chi", '\u03A7');
+		greekletters.put("chi", '\u03C7');
+		greekletters.put("Psi", '\u03A8');
+		greekletters.put("psi", '\u03C8');
+		greekletters.put("Omega", '\u03A9');
+		greekletters.put("omega", '\u03C9');
 
-		COMMON_VALUES.put('π', Expression.pi);
-
-		COMMON_VALUES.put('e', Expression.e);
-		COMMON_VALUES.put('i', Expression.i);
-
-		SHORTCUTS.put("log", new Shortcut(n -> n.length == 1 ? n[0].log(Expression.ten) : n[0].log(n[1]), 1, 2));
-		SHORTCUTS.put("ln", new Shortcut(Expression::ln));
+		functions.put("sin", e -> Trigonometric.sin(e[0]));
+		functions.put("ln", e -> e[0].ln());
+		functions.put("log", e -> e[0].log(e[1]));
+		functions.put("root", e -> e.length == 1 ? e[0].root(Expression.one_half) : e[0].root(e[1]));
 
 	}
 
@@ -52,239 +91,109 @@ public final class Simplifier {
 		}
 	}
 
-	public static final Expression simplify(final Object... expression) {
-
-		for (int i = 0; i < expression.length; i++) {
-
-			if (expression[i] instanceof Object[])
-				expression[i] = simplify((Object[]) expression[i]);
-			else if (expression[i]instanceof Double doubi)
-				expression[i] = parse(new BigDecimal(doubi));
-			else if (expression[i] instanceof java.lang.Number)
-				expression[i] = new Fraction(((java.lang.Number) expression[i]).longValue());
-			else if (expression[i] instanceof BigInteger)
-				expression[i] = new Fraction((BigInteger) expression[i]);
-			else if (expression[i]instanceof BigDecimal bd)
-				expression[i] = parse(bd);
-
-			else if (expression[i] instanceof Character)
-				switch (((Character) expression[i])) {
-				case '+':
-					expression[i] = Operator.ADD;
-					break;
-				case '-':
-					expression[i] = Operator.SUBTRACT;
-					break;
-				case '*':
-					expression[i] = Operator.MULTIPLY;
-					break;
-				case '/':
-					expression[i] = Operator.DIVIDE;
-					break;
-				case '^':
-					expression[i] = Operator.POW;
-					break;
-				default:
-					if (!Variable.isAcceptableName((Character) expression[i]))
-						throw new IllegalArgumentException(expression[i].toString());
-					else if (Simplifier.COMMON_VALUES.containsKey(expression[i]))
-						expression[i] = Simplifier.COMMON_VALUES.get(expression[i]);
-					else
-						expression[i] = new Variable(((Character) expression[i]).toString());
-
-				}
-
-			else if (!(expression[i] instanceof Symbol))
-				throw new IllegalArgumentException();
-		}
-
-		for (Object o : expression)
-			assert o instanceof Symbol;
-
-		return simplify((Symbol[]) expression);
+	private static final boolean isNumberCharacter(String s) {
+		for (char c : s.toCharArray())
+			if (Character.getType(c) != Character.DECIMAL_DIGIT_NUMBER && c != '.')
+				return false;
+		return true;
 	}
 
-	public static final Expression simplify(final Symbol... expression) {
-
-		ArrayList<Symbol> exprlist = new ArrayList<>(Arrays.asList(expression));
-
-		int index;
-		while ((index = exprlist.indexOf(Operator.POW)) != -1) {
-			exprlist.set(index - 1, ((Expression) exprlist.get(index - 1)).pow((Expression) exprlist.get(index + 1)));
-			exprlist.remove(index + 1);
-			exprlist.remove(index);
-		}
-
-		// The first and last elements in an expression must be operands,
-		// that is to say numbers, so they needn't be checked for whether they are a
-		// type of operand.
-		for (int i = 1; i < exprlist.size() - 1; i++)
-			if (exprlist.get(i) == Operator.MULTIPLY) {
-				exprlist.set(i - 1, ((Expression) exprlist.get(i - 1)).multiply((Expression) exprlist.get(i + 1)));
-				exprlist.remove(i + 1);
-				exprlist.remove(i--);
-			} else if (exprlist.get(i) == Operator.DIVIDE) {
-				exprlist.set(i - 1, ((Expression) exprlist.get(i - 1)).divide((Expression) exprlist.get(i + 1)));
-				exprlist.remove(i + 1);
-				exprlist.remove(i--);
-			}
-
-		for (int i = 1; i < exprlist.size() - 1; i++)
-			if (exprlist.get(i) == Operator.ADD) {
-				exprlist.set(i - 1, ((Expression) exprlist.get(i - 1)).add((Expression) exprlist.get(i + 1)));
-				exprlist.remove(i + 1);
-				exprlist.remove(i--);
-			} else if (exprlist.get(i) == Operator.SUBTRACT) {
-				exprlist.set(i - 1, ((Expression) exprlist.get(i - 1)).subtract((Expression) exprlist.get(i + 1)));
-				exprlist.remove(i + 1);
-				exprlist.remove(i--);
-			}
-
-		// If all goes well, the result should have a size() of one. If not, the user
-		// passed a bad expression.
-		if (exprlist.size() != 1) {
-			System.out.println(Arrays.toString(expression));
-			System.out.println(exprlist);
-
-			throw new IllegalArgumentException();
-		}
-
-		return (Expression) exprlist.get(0);
-	}
-
-	public static final Expression simplify(final String expression) {
-		return simplify(parse(expression));
+	// Returns whether this String represents an operator
+	private static boolean isOperator(String s) {
+		return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("^");
 	}
 
 	/**
-	 * The parse method converts the input String to an array of Symbols (commonly
-	 * referred to in documentation as an 'expression').
-	 * <p>
-	 * Expressions may contain any arithmetic operation, as well as logarithms and
-	 * other predefined and user defined operations and parenthetical grouping.
-	 * <p>
-	 * The parser converts all whitespace to the standard space and then either
-	 * removes all space, or removes redundant spacing, depending on the notation.
-	 * <p>
-	 * The parser converts numbers into lambda numbers, and converts variables
-	 * containing only valid characters into lambda variables.
-	 * <p>
-	 * This method is completely safe from attack.
+	 * Through strange and unholy mechanisms, this method converts a string into an
+	 * Expression.
 	 * 
-	 * @param notation the Notation to use
-	 * @param input    the input String to parse
-	 * 
-	 * 
-	 * @return the parsed expression
+	 * @param input
+	 * @return an Expression
 	 */
-	public static final Symbol[] parse(String input) {
-
-		if (input.contains("@"))
-			throw new IllegalArgumentException();
+	public static final Expression parse(String input) {
 
 		input = input.replaceAll("\\s", "");
 
+		// The results of the parenthetical expressions and functions in order.
 		ArrayList<Expression> replacedParentheticals = new ArrayList<>();
 
 		// Finds all parenthetical subexpressions in the unparsed String expression.
-		// Parentheses indicate either a predefined Shortcut, or grouping to change the
+		// Parentheses indicate either a predefined function (such as sin(x)), or
+		// grouping to change the
 		// order of operations.
-		while (input.indexOf('(') != -1)
-			try {
-				int start = -1;
-				if ((start = input.indexOf('(', start + 1)) != -1) {
+		while (input.indexOf('(') != -1) {
 
-					int dif = 1;
-					int end = start + 1;
+			int start = -1;
+			if ((start = input.indexOf('(', start + 1)) != -1) {
 
-					while (dif != 0)
-						if (input.charAt(end) == ')')
-							dif--;
-						else if (input.charAt(end++) == '(')
-							dif++;
+				// Finds the matching closing parenthesis to the opening. Since there can be
+				// multiple layers of parentheses, the correct one is found by counting up for
+				// each opening and down for each closing.
+				int dif = 1;
+				int end = start + 1;
 
-					int function_call_start = start - 1;
-
-					try {
-						while (Variable.isAcceptableName(input.charAt(function_call_start)))
-							function_call_start--;
-					} catch (StringIndexOutOfBoundsException e) {
-						// A StringIndexOutOfBoundsException may be thrown if the start of the function
-						// call is at the start of the function, making it an expected exception to
-						// receive.
-					}
-					function_call_start++;
-
-					// All shortcut call args are passed by putting them into parentheses directly
-					// following the shortcut's name, which emulates java static method calls.
-					if (function_call_start == start) {
-						replacedParentheticals.add(simplify(parse(input.substring(start + 1, end))));
-						input = input.replaceFirst(Pattern.quote(input.substring(start, end + 1)), "@");
-					} else {
-
-						Shortcut s = SHORTCUTS.get(input.substring(function_call_start, start));
-
-						Objects.requireNonNull(s);
-
-						replacedParentheticals.add(s.apply(Arrays.stream(input.substring(start + 1, end).split(","))
-								.map(Simplifier::parse).map(Simplifier::simplify).toArray(Expression[]::new)));
-
-						input = input.replaceFirst(Pattern.quote(input.substring(function_call_start, end + 1)), "@");
-					}
+				while (dif != 0) {
+					if (input.charAt(end) == ')')
+						dif--;
+					else if (input.charAt(end) == '(')
+						dif++;
+					end++;
 				}
-			} catch (StringIndexOutOfBoundsException e) {
-				throw new IllegalArgumentException(e);
+
+				String inParentheses = input.substring(start + 1, end - 1);
+
+				int function_call_start = start;
+
+				while (function_call_start != 0 && Variable.isAcceptableName(input.charAt(function_call_start - 1)))
+					function_call_start--;
+
+				String replacePattern = Pattern.quote(input.substring(function_call_start, end));
+
+				// All shortcut call args are passed by putting them into parentheses directly
+				// following the shortcut's name, which emulates java static method calls.
+				if (function_call_start == start) {
+					replacedParentheticals.add(parse(inParentheses));
+					input = input.replaceFirst(replacePattern, ")");
+				} else {
+
+					// Get function calls. Since some functions take more than one argument, the
+					// string within the parentheses that contains the argument(s) must be split
+					// into the arguments.
+					String[] functionargumentstrings = inParentheses.split(",");
+					Expression[] functionarguments = new Expression[functionargumentstrings.length];
+
+					for (int i = 0; i < functionarguments.length; i++)
+						functionarguments[i] = parse(functionargumentstrings[i]);
+
+					Expression result = functions.get(input.substring(function_call_start, start))
+							.apply(functionarguments);
+
+					replacedParentheticals.add(result);
+
+					input = input.replaceFirst(replacePattern, ")");
+				}
 			}
-
-		ArrayList<Object> expression = new ArrayList<>(input.length());
-
-		for (Character c : input.toCharArray())
-			expression.add(c.toString());
-
-		for (Expression o : replacedParentheticals) {
-			int lastfillerindex = expression.indexOf("@");
-			expression.set(lastfillerindex, o);
-			lastfillerindex = expression.subList(lastfillerindex + 1, expression.size()).indexOf("@");
 		}
 
-		expression.replaceAll(o -> {
+		// Converts this string into an arraylist of the constituent characters.
+		ArrayList<Object> expression = new ArrayList<>(Arrays.asList(input.split("")));
 
-			if (!(o instanceof String))
-				return o;
-
-			switch ((String) o) {
-			case "+":
-				return Operator.ADD;
-			case "-":
-				return Operator.SUBTRACT;
-			case "*":
-				return Operator.MULTIPLY;
-			case "/":
-				return Operator.DIVIDE;
-			case "^":
-				return Operator.POW;
-			case "âˆš":
-				return Operator.ROOT;
-			default:
-				return o;
-			}
-		});
+		// Within the arraylist, replace the ')' character with the expression it
+		// represented.
+		for (Expression o : replacedParentheticals)
+			expression.set(expression.indexOf(")"), o);
 
 		// Combined numbers with adjacent number characters to form large consecutive
 		// numeric Strings
+		for (int i = 1; i < expression.size(); i++) {
+			if (expression.get(i - 1)instanceof String str1 && expression.get(i)instanceof String str2
+					&& isNumberCharacter(str1) && isNumberCharacter(str2)) {
+				expression.set(i - 1, str1 + str2);
+				expression.remove(i--);
+			}
+		}
+
 		boolean found;
-		do {
-			found = false;
-
-			for (int i = 0; i < expression.size() - 1; i++)
-				if (expression.get(i)instanceof String exi && expression.get(i + 1)instanceof String exi1
-						&& isValidNumber(exi) && isValidNumber(exi1)) {
-					expression.set(i, exi + exi1);
-					expression.remove(i + 1);
-					found = true;
-				}
-
-		} while (found);
 
 		// Parses numbers into Decimals
 		for (int i = 0; i < expression.size(); i++)
@@ -307,9 +216,10 @@ public final class Simplifier {
 		} while (found);
 
 		for (int i = 0; i < expression.size(); i++)
-			try {
-				expression.set(i, new Variable((String) expression.get(i)));
-			} catch (ClassCastException c) {
+			if (expression.get(i)instanceof String variable && !isOperator(variable)) {
+				if (greekletters.containsKey(variable))
+					variable = Character.toString(greekletters.get(variable));
+				expression.set(i, new Variable(variable));
 			}
 
 		// If there is a number and a variable next to each other, insert a
@@ -317,59 +227,51 @@ public final class Simplifier {
 		// EG 4x -> 4*x
 		for (int i = 0; i < expression.size() - 1; i++)
 			if (expression.get(i)instanceof Expression exi && expression.get(i + 1)instanceof Expression exi1)
-				expression.add(i + 1, Operator.MULTIPLY);
-
-		expression.forEach(o -> {
-			if (!(o instanceof Symbol))
-				throw new IllegalArgumentException();
-		});
+				expression.add(i + 1, "*");
 
 		for (int i = 0; i < expression.size(); i++)
-			if (expression.get(i) == Operator.SUBTRACT && (i == 0 || expression.get(i - 1) instanceof Operator)
+			if (expression.get(i).equals("-") && (i == 0 || expression.get(i - 1)instanceof String i1 && isOperator(i1))
 					&& expression.get(i + 1)instanceof Expression exp) {
 				expression.set(i, exp.negate());
 				expression.remove(i + 1);
 			}
 
-		return expression.toArray(new Symbol[expression.size()]);
-	}
-
-	/*
-	 * Utility methods for parser.
-	 */
-
-	private static final boolean isValidNumber(String s) {
-		for (char c : s.toCharArray())
-			if (!NUMBER_CHARACTER.test(c))
-				return false;
-		return true;
-	}
-
-	/**
-	 * Adds the shortcut to the registry of shortcuts under the specified call
-	 * String. If a shortcut is already defined under the same call String, return
-	 * false.
-	 * 
-	 * @param call
-	 * @param action
-	 * @return whether the following call String has been registered to this action.
-	 */
-	public static final boolean define(String call, Shortcut action) {
-		if (SHORTCUTS.containsKey(call))
-			return false;
-		else {
-			synchronized (lock) {
-				SHORTCUTS.put(call, action);
-			}
-			return true;
+		int index;
+		while ((index = expression.indexOf("^")) != -1) {
+			expression.set(index - 1,
+					((Expression) expression.get(index - 1)).pow((Expression) expression.remove(index + 1)));
+			expression.remove(index);
 		}
-	}
 
-	public static synchronized final boolean undefine(String call) {
-		return SHORTCUTS.remove(call) != null;
-	}
+		// The first and last elements in an expression must be operands,
+		// that is to say numbers, so they needn't be checked for whether they are a
+		// type of operand.
+		for (int i = 1; i < expression.size() - 1; i++)
+			if (expression.get(i).equals("*")) {
+				expression.set(i - 1,
+						((Expression) expression.get(i - 1)).multiply((Expression) expression.remove(i + 1)));
+				expression.remove(i--);
+			} else if (expression.get(i).equals("/")) {
+				expression.set(i - 1,
+						((Expression) expression.get(i - 1)).divide((Expression) expression.remove(i + 1)));
+				expression.remove(i--);
+			}
 
-	private Simplifier() {
-		throw new UnsupportedOperationException();
+		for (int i = 1; i < expression.size() - 1; i++)
+			if (expression.get(i).equals("+")) {
+				expression.set(i - 1, ((Expression) expression.get(i - 1)).add((Expression) expression.remove(i + 1)));
+				expression.remove(i--);
+			} else if (expression.get(i).equals("-")) {
+				expression.set(i - 1,
+						((Expression) expression.get(i - 1)).subtract((Expression) expression.remove(i + 1)));
+				expression.remove(i--);
+			}
+
+		// If all goes well, the result should have a size() of one. If not, the user
+		// passed a bad expression.
+		if (expression.size() != 1)
+			throw new IllegalArgumentException();
+
+		return (Expression) expression.get(0);
 	}
 }
